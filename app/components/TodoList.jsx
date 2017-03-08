87a6e48ab0ec3b553478813e7ac83e12fff1a8e0
zerubabbel <achiappetta90@gmail.com>
2017-03-07 22:48:33 -0800

    add static todo list

---
+++ app/components/TodoList.jsx
@@ -0,0 +1,25 @@
+//Requiring the
+
+var React = require('react');
+var Todo = require('Todo');
+
+var TodoList = React.createClass({
+  render: function () {
+    var {todos} = this.props;
+    //mapping function that loops over all of the todos, and returns them with a key
+    var renderTodos = () => {
+      return todos.map((todo) => {
+        return (
+          <Todo key={todo.id} {...todo}/>
+        );
+      });
+    };
+    return (
+      <div>
+        {renderTodos()}
+      </div>
+    )
+  }
+});
+
+module.exports = TodoList;
